e9042db51d465e8c437448dac033c3ffe1e5e6df
Yanely <yanely_rmz@yahoo.com>
2018-02-11 21:49:15 -0500

    fixing data attribution

--- beachVue/scripts/beach.js
+++ beachVue/scripts/beach.js
@@ -3,31 +3,36 @@ const beach = new Vue({
 	data: {
 		activities: [
 			{
+				id: 1,
 				value: 'exercise',
 				text: 'Exercise',
 			},
 			{
+				id: 2,
 				value: 'tan',
 				text: 'Tan'
 			},
 			{
+				id: 3,
 				value: 'swim',
 				text: 'Swim'
 			},
 			{
+				id: 4,
 				value: 'picnic',
 				text: 'Picnic',
 			},
 			{
+				id: 5,
 				value: 'play volleyball',
 				text: 'Play volleyball',
 			}
 		],
+		checkedActivities: [],
 	},
 	methods: {
-		addActivity(event) {
-			console.log(event);
-			// this.thingsToDo.push(activity);
+		addActivity() {
+			console.log(this.checkedActivities);
 		},
 		removeActivity(event) {
 			console.log(event);
