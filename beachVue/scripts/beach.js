cdfd4d7c1adfcaf657b08ac66a239f7a6d1a2e51
Yanely <yanely_rmz@yahoo.com>
2018-02-12 21:02:41 -0500

    adding new activities with input

--- beachVue/scripts/beach.js
+++ beachVue/scripts/beach.js
@@ -29,16 +29,54 @@ const beach = new Vue({
 			}
 		],
 		checkedActivities: [],
+		newActivity: '',
 	},
 	methods: {
 		addActivity() {
-			console.log(this.checkedActivities);
+			const container = document.querySelector('.collection')
+			const addedInput = document.getElementById('newActivity')
+			const listItem = this.createListItem()
+			const input = this.createInput()
+			const label = this.createLabel()
+
+			listItem.appendChild(input)
+			listItem.appendChild(label)
+
+			container.appendChild(listItem)
+
+			// Clear input
+			addedInput.value = ''
 		},
 		removeActivity(event) {
+			const amount = 1;
+
 			this.checkedActivities.forEach(a => {
-				const index = this.activities.map(x => x.value).indexOf(a) + 1;
-				
+				const index = this.activities.map(x => x.value).indexOf(a)
+				this.activities.splice(index, amount)
 			})
+		},
+		createListItem() {
+			const el = document.createElement('li')
+
+			el.classList.add('collection-item')
+
+			return el
+		},
+		createInput() {
+			const el = document.createElement('input')
+
+			el.type = 'checkbox'
+			el.value = this.newActivity
+
+			return el
+		},
+		createLabel() {
+			const el = document.createElement('label')
+
+			el.classList.add('black-text')
+			el.innerText = this.newActivity
+
+			return el
 		}
 	}
 });
