70c79d4df16a9811ef4e6fbe0e126faba93d2b81
Yanely <yanely_rmz@yahoo.com>
2018-02-14 23:36:58 -0500

    only showing list if not empty

--- productVue/scripts/product.js
+++ productVue/scripts/product.js
@@ -1,15 +1,23 @@
+$('select').material_select();
+
 const product = new Vue({
 	el: '#products',
 	data: {
 		question: '',
 		response: '',
 		products: [],
+		selected: 'alphabetical begin',
+		sorting: [
+      { text: 'A - Z', value: 'alphabetical begin' },
+      { text: 'Z - A', value: 'alphabetical end' },
+      { text: 'Popularity', value: 'popularity' }
+    ]
 	},
 	watch: {
 		question(value) {
 			if (value.indexOf('products') > -1) {
 				this.response = 'Sure, I can list the products for you.'
-				fetch('https://hplussport.com/api/products', {
+				fetch('https://yxnely.github.io/vue-playground/productVue/data/products.json', {
 					method: 'GET'
 				}).then(res => res.json())
 				.then(res => {
