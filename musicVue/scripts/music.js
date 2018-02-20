dac60c0e8730153e958d20d628dfca464d620d05
Yanely <yanely_rmz@yahoo.com>
2018-02-19 20:31:04 -0500

    two different instances create different results

--- musicVue/scripts/music.js
+++ musicVue/scripts/music.js
@@ -2,9 +2,26 @@ Vue.component('global-component', {
     template: '<div>a global component</div>',
 })
 
+const LocalComponent = {
+    template: '<div @click="showNum()">a local component. My num is: {{num}}</div>',
+    data() {
+        return {
+            num: Math.random()
+        }
+    },
+    methods: {
+        showNum() {
+            alert(this.num * 100)
+        }
+    }
+}
+
 new Vue({
 	el: '#app',
 	data: {
         num: 42
     },
+    components: {
+        'local-component': LocalComponent
+    }
 })
