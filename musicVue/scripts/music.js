dc89fcb8dc066d5cdc5667c2e2ae19389dfa952b
Yanely <yanely_rmz@yahoo.com>
2018-02-22 22:35:08 -0500

    passing event to parent from child through $emit

--- musicVue/scripts/music.js
+++ musicVue/scripts/music.js
@@ -3,7 +3,7 @@ Vue.component('music-list', {
     <h2>{{ name }}</h2>\
     <slot>Week of 02/20/2018</slot>\
     <ul class="collection">\
-        <music-list-item :spot="spot" v-for="(spot, i) in music" :remove-method="remove.bind(this, i)" :key="music.id"></music-list-item>\
+        <music-list-item :spot="spot" v-for="(spot, i) in music" @remove="remove(i)" :key="music.id"></music-list-item>\
     </ul>\
     </div>',
     props: ['name', 'music'],
@@ -19,9 +19,15 @@ Vue.component('music-list-item', {
         <p>\
             <span>{{ spot.id }}</span> - {{ spot.name }} by {{ spot.artist }}\
         </p>\
-        <a @click="removeMethod">Hide this item</a>\
+        <a @click="requestRemove">Hide this item</a>\
     </li>',
-    props: ['spot', 'removeMethod']
+    props: ['spot'],
+    methods: {
+        // This way, the child component lets the parent know it wants to 'remove'
+        requestRemove() {
+            this.$emit('remove')
+        }
+    }
 })
 
 const musicApp = new Vue({
