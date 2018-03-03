5fd84463c88cff9883a9dc649ca21f2f01a40adb
Yanely <yanely_rmz@yahoo.com>
2018-03-03 10:54:56 -0500

    adding separate quote json

--- quoteVue/scripts/quote.js
+++ quoteVue/scripts/quote.js
@@ -1,32 +1,23 @@
-$('select').material_select();
-
 const product = new Vue({
 	el: '#quote',
 	data: {
-		quotes: [
-            {
-                text: 'There is only one happiness in this life, to love and be loved.',
-                author: 'George Sand',
-            },
-            {
-                text: 'Only I can change my life. No one can do it for me.',
-                author: 'Carol Burnett',
-            },
-            {
-                text: 'Life is 10% what happens to you and 90% how you react to it.',
-                author: 'Charles R. Swindoll',
-            },
-            {
-                text: 'I have found that if you love life, life will love you back.',
-                author: 'Arthur Rubinstein',
-            },
-        ],
+		quotes:[],
         quote: '',
     },
-    methods: {
-        getQuote() {
-            this.quote = this.quotes[Math.floor(Math.random() * Math.floor(4))];
+    created() {
+        fetch('https://yxnely.github.io/vue-playground/quoteVue/data/quotes.json', {
+            method: 'GET'
+        }).then(res => res.json())
+        .then(res => {
+            musicApp.musicChart = res
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    },
+    beforeMount: function() {
+		this.quote = this.quotes[Math.floor(Math.random() * Math.floor(4))];
             console.log(this.quote);
-        }
-    }
+	},
+    methods: {},
 });
