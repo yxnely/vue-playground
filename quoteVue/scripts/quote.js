a484c8c1f85b9aaf169dbe6b4d5f8c2db054f0be
Yanely <yanely_rmz@yahoo.com>
2018-03-03 11:07:49 -0500

    update to add more quotes

--- quoteVue/scripts/quote.js
+++ quoteVue/scripts/quote.js
@@ -1,19 +1,37 @@
 const product = new Vue({
 	el: '#quote',
 	data: {
-		quotes:[],
+		quotes: [
+            {
+                text: 'There is only one happiness in this life, to love and be loved.',
+                author: 'George Sand',
+            },
+            {
+                text: 'Only I can change my life. No one can do it for me.',
+                author: 'Carol Burnett',
+            },
+            {
+                text: 'Life is 10% what happens to you and 90% how you react to it.',
+                author: 'Charles R. Swindoll',
+            },
+            {
+                text: 'I have found that if you love life, life will love you back.',
+                author: 'Arthur Rubinstein',
+            },
+            {
+                text: 'Life isn\'t about finding yourself. Life is about creating yourself.',
+                author: 'George Bernard Shaw'
+            },
+            {
+                text: 'Don\'t cry because it\'s over, smile because it happened.',
+                author: 'Dr. Seuss'
+            },
+            {
+                text: 'You only live once, but if you do it right, once is enough.',
+                author: 'Mae West'
+            },
+        ],
         quote: '',
-    },
-    created() {
-        fetch('https://yxnely.github.io/vue-playground/quoteVue/data/quotes.json', {
-            method: 'GET'
-        }).then(res => res.json())
-        .then(res => {
-            musicApp.musicChart = res
-        })
-        .catch(err => {
-            console.log(err)
-        })
     },
     beforeMount: function() {
 		this.quote = this.quotes[Math.floor(Math.random() * Math.floor(4))];
