d94b4fad1a951bd8adc5792ec826d7312e2523d8
Yanely <yanely_rmz@yahoo.com>
2018-07-02 20:50:30 -0400

    adding style to quotes and adding more quotes

--- quoteVue/scripts/quote.js
+++ quoteVue/scripts/quote.js
@@ -10,8 +10,25 @@ const product = new Vue({
             method: 'GET'
         }).then(res => res.json())
         .then(res => {
-            this.quote = res[Math.floor(Math.random() * Math.floor(4))];
+            let chosen = Math.floor(Math.random() * Math.floor(res.length))
+            
+            this.quotes = res
+            this.quote = res[chosen]
         })
 	},
-    methods: {},
+    methods: {
+        getNextQuote() {
+            let chosen = this.getRandom()
+            const currentQuote = this.quotes.indexOf(this.quote)
+
+            while (chosen === currentQuote) {
+                chosen = this.getRandom() 
+            }
+
+            this.quote = this.quotes[chosen]
+        },
+        getRandom() {
+            return Math.floor(Math.random() * Math.floor(this.quotes.length));
+        }
+    },
 });
